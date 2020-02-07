4eaad7c753aa0ff1a8b21c6d7760ac93e0e03c47
Martin Larsson <martin.99.larsson@telia.com>
2020-02-07 14:52:00 +0100

    Living prototype

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,147 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" data-app>
+    <header2 :user="user" v-on:logout="user.isLoggedIn = false"/>
+    <div id="center">
+      <v-card id="card">
+        <v-expand-transition>
+          <login v-if="!user.isLoggedIn" v-on:login="login"/>
+        </v-expand-transition>
+        <v-expand-transition>
+          <itemsView v-if="user.isLoggedIn && !viewItem" :items="items" v-on:view="viewItem = $event" />
+        </v-expand-transition>
+        <v-expand-transition>
+          <itemView v-if="user.isLoggedIn && viewItem" :item="viewItem" v-on:close="viewItem = false"/>
+        </v-expand-transition>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import header2 from "./components/Header";
+import login from "./components/Login";
+import itemsView from "./components/Items";
+import itemView from './components/Item'
 
 export default {
-  name: 'app',
+  name: "App",
+
   components: {
-    HelloWorld
+    header2,
+    login,
+    itemsView,
+    itemView
+  },
+
+  data: () => ({
+    user: {
+      name: "DemoClientAccount",
+      isLoggedIn: false
+    },
+    viewItem: false,
+    items: []
+  }),
+  methods: {
+    login() {
+      var vm = this
+      vm.user.isLoggedIn = true
+      vm.viewItem = false
+    },
+  },
+  mounted() {
+    var vm = this;
+    vm.items.push({
+      icon: "a_chair.jpg",
+      name: "A Chair",
+      status: "Complete",
+      statusIcon: "check",
+      modelLink: "./Astronaut.glb",
+      comments: [
+        {
+          name: "DemoClientAccount",
+          type: "client",
+          message: "link to item: http://DemoClient.com/a_chair"
+        },
+        {
+          name: "QA",
+          type: "qa",
+          message: "all done here!"
+        }
+      ]
+    });
+    vm.items.push({
+      icon: "a_table.jpg",
+      name: "A Table",
+      status: "More info required",
+      statusIcon: "warning",
+      comments: [
+        {
+          name: "QA",
+          type: "qa",
+          message: "We need more info on the materials"
+        }
+      ]
+    });
+    for (let i = 0; i < 20; i++) {
+      vm.items.push({
+        icon: "a_sofa.jpg",
+        name: "A Sofa",
+        status: "In Development",
+        statusIcon: "",
+        comments: []
+      })
+    }
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  background-color: grey;
+  height: 100vh;
+  font-family: "Roboto";
+}
+
+p,
+a,
+h1 {
+  color: grey;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: grey;
+}
+
+#center {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+#card {
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  padding: 40px;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
 }
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="header">
+    <div class="row">
+      <img src="charpstar.png" id="logo" />
+      <p>Charpstar 3D model platform thing™</p>
+    </div>
+    <div v-if="user.isLoggedIn">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text id="menuButton" v-on="on"><p>{{user.name}}</p><i class="material-icons" id="acountIcon">account_circle</i></v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index" @click="item.click">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header2",
+  props: {
+    user: { type: Object, required: true }
+  },
+  data: () => ({
+    items: []
+  }),
+  methods: {
+    logout() {
+      this.$emit('logout')
+    }
+  },
+  mounted() {
+    var vm = this
+    vm.items = [
+      { title: "Settings", click: () => {} },
+      { title: "Log out",  click: vm.logout },
+      { title: "Support",  click: () => {} }
+    ]
+  }
+};
+</script>
+
+
+<style lang="scss">
+#header {
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  background-color: white;
+  justify-content: space-between;
+  .row {
+    flex-grow: 0;
+  }
+  p {
+    font-size: 32px;
+    padding: 0;
+    margin: 0;
+    margin-top: auto;
+    margin-bottom: auto;
+    color: grey;
+  }
+}
+
+#logo {
+  max-width: 70px;
+}
+
+#acountIcon {
+  color: grey;
+  font-size: 60px;
+}
+#menuButton {
+  height: 70px;
+}
+</style>
