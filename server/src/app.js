a0997f9d48d086ab06e5530157a4cd18e6c5bbf1
Vidarr <luckri@hotmail.se>
2020-04-12 22:56:24 +0200

    cors fix 5

--- server/src/app.js
+++ server/src/app.js
@@ -60,8 +60,8 @@ app.use(session({
 initUserCreationService();
 
 app.all('/', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
