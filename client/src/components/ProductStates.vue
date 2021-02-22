25072cc3e2ceda2f01542ba8d1566f0adb80df67
Evangelia Ioannou <evievi@outlook.com>
2021-02-22 09:46:52 +0100

    Change layout in product states expansion panel #5

--- client/src/components/ProductStates.vue
+++ client/src/components/ProductStates.vue
@@ -1,49 +1,74 @@
 <template>
   <div>
-    Total products: {{total}}
-    <p v-for="state in orderedstates" :key="state.stateafter">
-      {{statusMessage(state)}} : {{state.count}} / {{total}}
-      <v-img 
-        :src="iconFromAccount(state.stateafter)"
-        class="custom-icon"
-      />
-    </p>
+    <h2>Total products: {{total}}</h2> 
+    <v-layout v-for="state in orderedstates" :key="state.stateafter" >
+          <div id="state-icon">
+            <v-img :src="iconFromAccount(state.stateafter)" class="custom-icon" />
+          </div>
+          <div id="state-explanation">
+            <p>{{statusMessage(state)}}</p>
+            <p>{{state.count}} / {{total}}</p>
+          </div>
+    </v-layout>
   </div>
 </template>
 
 <script>
-import backend from '../backend'
+  import backend from '../backend'
 
-export default {
+  export default {
     props: {
-        account: {type: Object, required: true},
-        orderedstates: {type: Array, required: true},
-        total: {type: Number, required: true},
-        baricons: {type: Object, required: true},
-        clientbaricons: {type: Object, required: true}
+      account: {
+        type: Object,
+        required: true
+      },
+      orderedstates: {
+        type: Array,
+        required: true
+      },
+      total: {
+        type: Number,
+        required: true
+      },
+      baricons: {
+        type: Object,
+        required: true
+      },
+      clientbaricons: {
+        type: Object,
+        required: true
+      }
     },
-    
+
 
     methods: {
-        statusMessage(state) {
-            return backend.messageFromStatus(state.stateafter, this.account.usertype)
-        },
+      statusMessage(state) {
+        return backend.messageFromStatus(state.stateafter, this.account.usertype)
+      },
 
-        iconFromAccount(state) {
-            if(this.account.usertype == 'Client') {
-                return this.clientbaricons[state]
-            }
-            return this.baricons[state]
+      iconFromAccount(state) {
+        if (this.account.usertype == 'Client') {
+          return this.clientbaricons[state]
         }
+        return this.baricons[state]
+      }
     }
 
-}
+  }
 </script>
 
 <style scoped>
-    .custom-icon {
-        height: 40px;
-        width: 40px
-    }
+  .custom-icon {
+    height: 40px;
+    width: 40px
+  }
 
+  h2 {
+    margin-bottom: 1em;
+  }
+
+  #state-icon {
+    margin-right: 1em;
+    margin-bottom: 1.5em;
+  }
 </style>
