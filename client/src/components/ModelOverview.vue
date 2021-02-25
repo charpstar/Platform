b225d81e64569912712ede74f357eb54e770f0f2
Evangelia Ioannou <evievi@outlook.com>
2021-02-25 15:43:42 +0100

    Created new component to display models' list and details at same page

---
+++ client/src/components/ModelOverview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flexrow">
+        <model-list-view />
+        <model-view />
+    </div>
+</template>
+
+<script>
+import ModelListView from './ModelListView.vue'
+import ModelView from './ModelView.vue'
+
+export default {
+    components: {
+        ModelListView,
+        ModelView
+    }
+
+}
+</script>
+
+<style>
+
+</style>
